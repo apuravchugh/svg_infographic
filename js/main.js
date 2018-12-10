f866b0f231500acc8d83bab3dcf4f56b8bb3215a
apuravchugh <39134035+apuravchugh@users.noreply.github.com>
2018-12-09 19:06:17 -0500

    late upload
    all file are uploaded

---
+++ js/main.js
@@ -0,0 +1,151 @@
+
+/* lab animation*/
+TweenMax.to("#labrador_x5F_retriever", 0.001, {x:580});
+TweenMax.to("#lab_x5F_text", 0.001, {opacity:0});
+function moveDog(){
+TweenMax.to("#labrador_x5F_retriever", 1, {x:0});
+TweenMax.to("#lab_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#labrador_x5F_retriever").addEventListener("mouseover", moveDog)
+
+/*sherpherd aniamtion  */
+TweenMax.to("#greman_x5F_shepherd", 0.001, {x:550});
+TweenMax.to("#german_x5F_text", 0.001, {opacity:0});
+function moveDog2(){
+TweenMax.to("#greman_x5F_shepherd", 1, {x:0});
+TweenMax.to("#german_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#greman_x5F_shepherd").addEventListener("mouseover", moveDog2)
+
+/*basset hound  */
+TweenMax.to("#basset_x5F_hound", 0.001, {x:530});
+TweenMax.to("#basset_x5F_text", 0.001, {opacity:0});
+function moveDog3(){
+TweenMax.to("#basset_x5F_hound", 1, {x:0});
+TweenMax.to("#basset_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#basset_x5F_hound").addEventListener("mouseover", moveDog3)
+
+/*golden retriever */
+TweenMax.to("#golden_x5F_retriever", 0.001, {x:550});
+TweenMax.to("#golden_x5F_text", 0.001, {opacity:0});
+function moveDog4(){
+TweenMax.to("#golden_x5F_retriever", 1, {x:0});
+TweenMax.to("#golden_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#golden_x5F_retriever").addEventListener("mouseover", moveDog4)
+
+/* beagle */
+TweenMax.to("#beagle", 0.001, {x:550});
+TweenMax.to("#beagle_x5F_text", 0.001, {opacity:0});
+function moveDog5(){
+TweenMax.to("#beagle", 1, {x:0});
+TweenMax.to("#beagle_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#beagle").addEventListener("mouseover", moveDog5)
+
+/* boxer */
+TweenMax.to("#boxer", 0.001, {x:500});
+TweenMax.to("#boxer_x5F_text", 0.001, {opacity:0});
+function moveDog6(){
+TweenMax.to("#boxer", 1, {x:0});
+TweenMax.to("#boxer_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#boxer").addEventListener("mouseover", moveDog6)
+
+/* bulldog */
+TweenMax.to("#bulldog", 0.001, {x:550});
+TweenMax.to("#bulldog_x5F_text", 0.001, {opacity:0});
+function moveDog7(){
+TweenMax.to("#bulldog", 1, {x:0});
+TweenMax.to("#bulldog_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#bulldog").addEventListener("mouseover", moveDog7)
+
+/* dachshund */
+TweenMax.to("#dachshund", 0.001, {x:550});
+TweenMax.to("#dachshund_x5F_text", 0.001, {opacity:0});
+function moveDog8(){
+TweenMax.to("#dachshund", 1, {x:0});
+TweenMax.to("#dachshund_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#dachshund").addEventListener("mouseover", moveDog8)
+
+/* poodle */
+TweenMax.to("#poodle", 0.001, {x:600});
+TweenMax.to("#poodle_x5F_text", 0.001, {opacity:0});
+function moveDog9(){
+TweenMax.to("#poodle", 1, {x:0});
+TweenMax.to("#poodle_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#poodle").addEventListener("mouseover", moveDog9)
+
+/* dalmation */
+TweenMax.to("#dalmation", 0.001, {x:470});
+TweenMax.to("#dalmation_x5F_text", 0.001, {opacity:0});
+function moveDog10(){
+TweenMax.to("#dalmation", 1, {x:0});
+TweenMax.to("#dalmation_x5F_text", 1, {opacity:1,delay:1});
+}
+document.querySelector("#dalmation").addEventListener("mouseover", moveDog10)
+
+
+
+
+/*buttons*/
+
+/* dolar sign */
+TweenMax.to("#dollar_x5F_text", 0.001, {opacity:0});
+function hide(){
+TweenMax.to("#dollar_x5F_text", 0.5, {opacity:1,delay:0});
+}
+document.querySelector("#dollar_x5F_text").addEventListener("mouseover", hide)
+
+TweenMax.to("#dollar_x5F_sign", 0.001, {opacity:1});
+function show1(){
+TweenMax.to("#dollar_x5F_sign", 0.5, {opacity:0,delay:0});
+}
+document.querySelector("#dollar_x5F_sign").addEventListener("mouseover", show1)
+
+
+/*american flag */
+TweenMax.to("#us_x5F_text_1_", 0.001, {opacity:0});
+function hide2(){
+TweenMax.to("#us_x5F_text_1_", 0.5, {opacity:1,delay:0});
+}
+document.querySelector("#us_x5F_text_1_").addEventListener("mouseover", hide2)
+
+TweenMax.to("#us_x5F_icon", 0.001, {opacity:1});
+function show2(){
+TweenMax.to("#us_x5F_icon", 0.5, {opacity:0,delay:0});
+}
+document.querySelector("#us_x5F_icon").addEventListener("mouseover", show2)
+
+
+/* Dog paw */
+TweenMax.to("#paw_x5F_text_1_", 0.001, {opacity:0});
+function hide3(){
+TweenMax.to("#paw_x5F_text_1_", 0.5, {opacity:1,delay:0});
+}
+document.querySelector("#paw_x5F_text_1_").addEventListener("mouseover", hide3)
+
+TweenMax.to("#pawicon", 0.001, {opacity:1});
+function show3(){
+TweenMax.to("#pawicon", 0.5, {opacity:0,delay:0});
+}
+document.querySelector("#pawicon").addEventListener("mouseover", show3)
+
+
+
+/* age */
+TweenMax.to("#age_x5F_text", 0.001, {opacity:0});
+function hide4(){
+TweenMax.to("#age_x5F_text", 0.5, {opacity:1,delay:0});
+}
+document.querySelector("#age_x5F_text").addEventListener("mouseover", hide4)
+
+TweenMax.to("#age_x5F_icon_1_", 0.001, {opacity:1});
+function show4(){
+TweenMax.to("#age_x5F_icon_1_", 0.5, {opacity:0,delay:0});
+}
+document.querySelector("#age_x5F_icon_1_").addEventListener("mouseover", show4)
